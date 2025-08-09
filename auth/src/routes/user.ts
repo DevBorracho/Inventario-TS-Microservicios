57c235c335997670da387dc93425f1c1201d90ae
Jhonathan Feliz <sakurakojisakura008@gmail.com>
2025-08-09 10:24:16 -0400

    servicio hecho solo falta cambiar el verify token a el gateway y los otros services

--- auth/src/routes/user.ts
+++ auth/src/routes/user.ts
@@ -3,7 +3,7 @@ import verifyToken from "../middlewares/verifyToken.ts";
 import type { AuthenticatedRequest } from "../middlewares/verifyToken.ts";
 import createToken from "../utils/jwt.ts";
 import User from "../models/user.ts";
-import { hash } from "bcrypt";
+import { compare, hash } from "bcrypt";
 const router = Router();
 
 router.get("/profile", verifyToken, async (req: AuthenticatedRequest, res) => {
@@ -19,22 +19,42 @@ router.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
   try {
     const passwordHash: string = await hash(password, 8);
+    const user = await User.findOne({ email: email });
+    if (user) return res.status(400).json({ msg: "el usuario ya existe" });
     const newUser = new User({
       username: username,
       email: email,
       password: passwordHash,
     });
     await newUser.save();
+    const token = await createToken(newUser.id);
     return res.json({
       id: newUser._id,
       username: newUser.username,
       email: newUser.email,
+      token: token,
     });
   } catch (error) {
     return res.json(error);
   }
 });
-router.post("/login", async (req, res) => {});
-router.post("/logout", verifyToken, async (req, res) => {});
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(404).json({ msg: "email invalido" });
+    const userMatch = await compare(password, user.password as string);
+    if (!userMatch) return res.json({ msg: "password invalida" });
+    const token = await createToken(user.id);
+    return res.json({
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      token: token,
+    });
+  } catch (error) {
+    return res.json(error);
+  }
+});
 
 export default router;
